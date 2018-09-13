af520e17ecba4b42eb62536b7569638045ee80f5
aliciaketzueki <aliciaketzueki@gmail.com>
2018-09-13 22:44:37 +0300

    Добавляет открытие\закрытие окна персонажа

--- js/setup.js
+++ js/setup.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// Задача 1
+// Задача 1.1
 var removeClass = function (className, removedClass) {
   document.querySelector(className).classList.remove(removedClass);
 };
 removeClass('.setup', 'hidden');
 
-// Задача 2
+// Задача 1.2
 var firstNameArr = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surnameArr = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColorArr = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -34,7 +34,7 @@ var createArray = function (arr) {
 var heroes = [];
 createArray(heroes);
 
-// Задача 3
+// Задача 1.3
 var similarList = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
@@ -46,7 +46,7 @@ var renderHeroes = function (arr) {
   return similarItem;
 };
 
-// Задача 4
+// Задача 1.4
 var addElements = function (elements) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < elements.length; i++) {
@@ -56,5 +56,54 @@ var addElements = function (elements) {
 };
 addElements(heroes);
 
-// Задача 5
+// Задача 1.5
 removeClass('.setup-similar', 'hidden');
+
+// Задача 2.1
+var ESC_KEYDOWN = 27;
+var ENTER_KEYDOWN = 13;
+
+var openSetupWindow = function () {
+  setup.classList.remove('hidden');
+};
+
+var closeSetupWindow = function () {
+  setup.classList.add('hidden');
+};
+
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
+var setupClose = setup.querySelector('.setup-close');
+
+setupOpen.addEventListener('click', function () {
+  openSetupWindow();
+
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ESC_KEYDOWN) {
+      closeSetupWindow();
+    }
+  });
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYDOWN) {
+    openSetupWindow();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closeSetupWindow();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYDOWN) {
+    closeSetupWindow();
+  }
+});
+
+/* 
+Если диалог открыт, нажатие на кнопку «Сохранить» приводит к отправке формы
+Если диалог открыт и фокус находится на кнопке «Сохранить», нажатие на ENTER приводит к отправке формы
+*/
+
+// Задача 2.2
